14c5b0a8adff26db02d58236938088a4676daf3c
wpalafox <wpalafox@gmail.com>
2019-08-05 18:26:59 -0500

    starting tribute page

--- tribute-page/src/App.js
+++ tribute-page/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="title">Playboi Carti</div>
+      <img src="https://lastfm-img2.akamaized.net/i/u/ar0/27bce3a8fb291a96d6daa1de27ca8057.jpg" alt="Playboi Carti 2"></img>
     </div>
   );
 }
